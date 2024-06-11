02217b05a3267cdcdf6856a111456274b07426f4
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-06-11 23:01:15 +0200

    Initial comit

---
+++ src/test/java/com/foodfinder/food_finder/FoodFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodfinder.food_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
