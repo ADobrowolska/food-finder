f0a5d5ad3bb9b7d65b1a240d33cab25aceefaf8c
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-06-12 22:32:18 +0200

    feat: Security configuration

---
+++ src/main/java/com/foodfinder/food_finder/configuration/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.foodfinder.food_finder.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withUsername("AdminAnna")
+                .password(passwordEncoder().encode("123"))
+                .roles("ADMIN", "USER")
+                .build();
+
+        UserDetails user = User.withUsername("UserJan")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/welcome", "/error").permitAll())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/user/**").authenticated())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/admin/**").authenticated())
+                .formLogin(withDefaults())
+                .logout(auth -> auth.deleteCookies("JSESSIONID")
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout")))
+                .build();
+    }
+
+
+}
---
+++ src/main/java/com/foodfinder/food_finder/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.foodfinder.food_finder.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "WELCOME";
+    }
+
+    @GetMapping("/user/userProfile")
+    public String userProfile() {
+        return "Welcome to User Profile";
+    }
+
+    @GetMapping("/admin/adminProfile")
+    public String adminProfile() {
+        return "Welcome to Admin Profile";
+    }
+
+
+
+}
