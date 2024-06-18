576a580906f9c2354433be0656c598f99711acb3
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-06-18 20:37:06 +0200

    feat: add db version V01 - create table

---
+++ src/main/resources/db.version/V01.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+--changeset adobrowolska:1
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(250) NOT NULL,
+    last_name VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    password VARCHAR(250) NOT NULL,
+    role VARCHAR(250) NOT NULL
+);
